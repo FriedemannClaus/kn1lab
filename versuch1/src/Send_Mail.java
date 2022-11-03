540bff527370e8eb87253d428cef569f4ffb74f2
FriedemannClaus <FriedemannClaus@users.noreply.github.com>
2022-11-03 10:37:45 +0100

    Versuch 1 solution main branch

--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -14,7 +15,28 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
-			// your code here
+			String msgSubject = "Die beste BESTE Mail";
+			String msgRecipient = "labrat@localhost";
+			String msgTxt = "Es gibt nichts zu sagen. ";
+
+			try {
+				Properties prop = new Properties();
+				prop.put("mail.smtp.host", "localhost");
+				prop.put("mail.smtp.port", "25");
+				prop.put("mail.from", "ich_bin@bundeskanzler.de");
+				Session session = Session.getInstance(prop);
+
+				Message msg = new MimeMessage(session);
+				msg.setSubject(msgSubject);
+				msg.setSentDate(new Date());
+				msg.setFrom();
+				msg.setRecipients(Message.RecipientType.TO,
+						InternetAddress.parse(msgRecipient, false));
+				msg.setText(msgTxt);
+				Transport.send(msg);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
