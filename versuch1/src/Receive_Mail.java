c5fd50b111d3e81e61832ad39add7a445559adf0
FriedemannClaus <FriedemannClaus@users.noreply.github.com>
2022-11-03 10:36:30 +0100

    Versuch 1 Solution

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -11,7 +11,31 @@ public class Receive_Mail {
 	
 	public static void fetchMail() {
 		try {
-			// your code here
+			Properties properties = new Properties();
+			properties.put("mail.pop3.host", "localhost");
+			properties.put("mail.pop3.port", "110");
+			properties.put("mail.pop3.starttls.enable", "false");
+			properties.put("mail.store.protocol", "pop3");
+			Session session = Session.getInstance(properties);
+			Store store = session.getStore("pop3");
+			store.connect("localhost", "labrat", "kn1lab");
+			Folder folder = store.getFolder("INBOX");
+			folder.open(Folder.READ_ONLY);
+
+			Message[] messages = folder.getMessages();
+			System.out.println("Nachrichtenanzahl: " + messages.length);
+			for (int i = 0; i < messages.length; i++) {
+				StringBuilder builder = new StringBuilder(i + ": ");
+				builder.append("Von: ")
+						.append(messages[i].getFrom()[0].toString())
+						.append(" Betreff: ")
+						.append(messages[i].getSubject())
+						.append(" Versanddatum: ")
+						.append(messages[i].getSentDate())
+						.append(" Inhalt: ")
+						.append(messages[i].getContent().toString());
+				System.out.println(builder);
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
